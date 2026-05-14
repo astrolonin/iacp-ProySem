30294d175d6cf83c33bc5f9f52673a112b914b53
astrolonin <landresvrivas@gmail.com>
2026-05-14 17:01:09 -0400

    thursday may 14th

--- main.cpp
+++ main.cpp
@@ -2,20 +2,23 @@
 #include "svd_seq.hpp"
 
 int main(){
-    int T = 4; int D = 3;
+    int T = 6; int D = 5;
 
     MatrixHCI X = randMatrix(T, D);
+    MatrixHCI W = X;
     printMatrix(X, "Experimento");
 
     MatrixHCI V(D,D);
     for (int i = 0; i < D; ++i){V(i,i) = 1.0;}
 
-    SVDSequential(X, V);
+    SVDSequential(W, V);
 
     
-    printMatrix(X, "X Ortogonal (W)");
+    printMatrix(W, "X Ortogonal (W)");
     printMatrix(V, "Matrix V final");
 
-    printMatrix(X.transpose()*X, "Deberia ser diagonal");
+    printMatrix(W.transpose()*W, "Deberia ser diagonal");
+    printMatrix(W*V.transpose(), "Deberia ser X");
+    
     return 0;
 }
--- svd_seq.hpp
+++ svd_seq.hpp
@@ -6,16 +6,16 @@ int sign(T val){
     //return (val >= T(0) ? 1 : -1);
 }
 
-void SVDSequential(MatrixHCI& X, MatrixHCI& V, double epsilon = 1e-5){
+void SVDSequential(MatrixHCI& X, MatrixHCI& V, double epsilon = 1e-7){
     int T = X.rows, D = X.cols;
     bool any_rots = true;
-    int sweeps = 0, max_sweeps = 1000;
+    int sweeps = 0, max_sweeps = 100000;
 
     while (any_rots && sweeps < max_sweeps){
         any_rots = false;
         for (int i = 0; i < D-1; ++i){
             for (int j = 0; j < D; ++j){
-                
+                 
                 double a = 0.0, b = 0.0, c = 0.0;
                 for (int r = 0; r < T; ++r){
                     a += X(r, i) * X(r, i);
@@ -23,7 +23,7 @@ void SVDSequential(MatrixHCI& X, MatrixHCI& V, double epsilon = 1e-5){
                     c += X(r, i) * X(r, j); 
                 }
                 
-                if (a == 0.0 || b == 0.0) continue;
+                if (a < 1e-12|| b < 1e-12) continue;
                 double corr = std::abs(c) / std::sqrt(a*b);
 
                 if (corr > epsilon){
@@ -37,15 +37,15 @@ void SVDSequential(MatrixHCI& X, MatrixHCI& V, double epsilon = 1e-5){
                     for (int r = 0; r < T; ++r){
                         double x_i = X(r, i);
                         double x_j = X(r, j);
-                        X(r, i) =  c_rot * x_i + s_rot * x_j;
-                        X(r, j) = -s_rot * x_i + c_rot * x_j; 
+                        X(r, i) = c_rot * x_i - s_rot * x_j;
+                        X(r, j) = s_rot * x_i + c_rot * x_j; 
                     }
 
                     for (int r = 0; r < D; ++r){
                         double v_i = V(r, i);
                         double v_j = V(r, j);
-                        V(r, i) =  c_rot * v_i + s_rot * v_j;
-                        V(r, j) = -s_rot * v_i + c_rot * v_j;
+                        V(r, i) =  c_rot * v_i - s_rot * v_j;
+                        V(r, j) =  s_rot * v_i + c_rot * v_j;
                     }
                 }
             }
