c1758f02908588f6c72da12df3184ae50d1e3385
astrolonin <landresvrivas@gmail.com>
2026-05-18 15:05:15 -0400

    fixed convergence problem

--- matrix_hci.hpp
+++ matrix_hci.hpp
@@ -1,79 +1,82 @@
 #ifndef MATRIX
 #define MATRIX
 
-#include <iostream>
-#include <array>
 #include <iomanip>
+#include <iostream>
 #include <random>
-#include <vector>
 #include <string>
+#include <vector>
+
 
 struct MatrixHCI {
-    int rows; // T rows (frames)
-    int cols; // D columns (pixels)
-    std::vector<double> data;
+  int rows; // T rows (frames)
+  int cols; // D columns (pixels)
+  std::vector<double> data;
 
-    // Constructor allocating the flat vector
-    MatrixHCI(int r, int c) : rows(r), cols(c), data(r*c, 0.0) {}
+  // Constructor allocating the flat vector
+  MatrixHCI(int r, int c) : rows(r), cols(c), data(r * c, 0.0) {}
 
-    // Column-major indexing
-    double& operator()(int r, int c) {return data[c * rows + r];}
+  // Column-major indexing
+  double &operator()(int r, int c) { return data[c * rows + r]; }
 
-    // Column-major indexing
-    const double& operator()(int r, int c) const {return data[c * rows + r];}
+  // Column-major indexing
+  const double &operator()(int r, int c) const { return data[c * rows + r]; }
 
-    MatrixHCI operator*(const MatrixHCI& other) const{
-        if (cols != other.rows) {
-            throw std::invalid_argument("Matrix dimensions mismatched.");
-        }
-        MatrixHCI result(rows, other.cols);
-
-        for (int r = 0; r < rows; ++r){
-            for (int c = 0; c < other.cols; ++c){
-                double sum = 0.0;
-                for (int k = 0; k < cols; ++k){
-                    sum += (*this)(r,k) * other(k,c);
-                }
-                result(r,c) = sum;
-            }
-        }
-        return result;
+  MatrixHCI operator*(const MatrixHCI &other) const {
+    if (cols != other.rows) {
+      throw std::invalid_argument("Matrix dimensions mismatched.");
     }
+    MatrixHCI result(rows, other.cols);
 
-    MatrixHCI transpose() const {
-        MatrixHCI result(cols, rows);
-        for (int r = 0; r < rows; ++r){
-            for (int c = 0; c < cols; ++c){
-                result(c,r) = (*this)(r,c);
-            }
+    for (int r = 0; r < rows; ++r) {
+      for (int c = 0; c < other.cols; ++c) {
+        double sum = 0.0;
+        for (int k = 0; k < cols; ++k) {
+          sum += (*this)(r, k) * other(k, c);
         }
-        return result;
+        result(r, c) = sum;
+      }
     }
+    return result;
+  }
+
+  MatrixHCI transpose() const {
+    MatrixHCI result(cols, rows);
+    for (int r = 0; r < rows; ++r) {
+      for (int c = 0; c < cols; ++c) {
+        result(c, r) = (*this)(r, c);
+      }
+    }
+    return result;
+  }
 };
 
-void printMatrix(const MatrixHCI& m, const std::string& name){
-    std::cout << "--- " << name << " (" << m.rows << "x" << m.cols << ") ---" << std::endl;
-    for (int r = 0; r < m.rows; ++r){
-        for (int c = 0; c < m.cols; ++c){
-            std::cout << std::setw(8) << std::setprecision(10) << std::fixed << m(r,c) << " ";
-        }
-        std::cout << std::endl;
+inline void printMatrix(const MatrixHCI &m, const std::string &name) {
+  std::cout << "--- " << name << " (" << m.rows << "x" << m.cols << ") ---"
+            << std::endl;
+  for (int r = 0; r < m.rows; ++r) {
+    for (int c = 0; c < m.cols; ++c) {
+      std::cout << std::setw(8) << std::setprecision(10) << std::fixed
+                << m(r, c) << " ";
     }
-    std::cout << std::endl;   
+    std::cout << std::endl;
+  }
+  std::cout << std::endl;
 }
 
-MatrixHCI randMatrix(int rows, int cols){
-    MatrixHCI m(rows, cols);
+inline MatrixHCI randMatrix(int rows, int cols) {
+  MatrixHCI m(rows, cols);
 
-    std::random_device rd; std::mt19937 gen(rd());
-    std::uniform_real_distribution<> dis (-10.0,10);
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<> dis(-10.0, 10);
 
-    for (int r = 0; r < rows; ++r){
-        for (int c = 0; c < cols; ++c){
-            m(r, c) = dis(gen);
-        }
+  for (int r = 0; r < rows; ++r) {
+    for (int c = 0; c < cols; ++c) {
+      m(r, c) = dis(gen);
     }
-    return m;
+  }
+  return m;
 }
 
 #endif
--- svd_seq.hpp
+++ svd_seq.hpp
@@ -1,72 +1,76 @@
 #include "matrix_hci.hpp"
 
-template <typename T>
-int sign(T val){
-    return (T(0) < val) - (val < T(0));
-    //return (val >= T(0) ? 1 : -1);
+template <typename T> int sign(T val) {
+  return (T(0) < val) - (val < T(0));
+  // return (val >= T(0) ? 1 : -1);
 }
 
-void SVDSequential(MatrixHCI& X, MatrixHCI& V, double epsilon = 1e-10){
-    int T = X.rows, D = X.cols;
-    bool any_rots = true;
-    int sweeps = 0, max_sweeps = 100000;
+inline void SVDSequential(MatrixHCI &X, MatrixHCI &V, double epsilon = 1e-10) {
+  int T = X.rows, D = X.cols;
+  bool any_rots = true;
+  int sweeps = 0, max_sweeps = 500;
 
-    while (any_rots && sweeps < max_sweeps){
-        any_rots = false;
-        for (int i = 0; i < D-1; ++i){
-            for (int j = 0; j < D; ++j){
-                 
-                double a = 0.0, b = 0.0, c = 0.0;
-                for (int r = 0; r < T; ++r){
-                    a += X(r, i) * X(r, i);
-                    b += X(r, j) * X(r, j);
-                    c += X(r, i) * X(r, j); 
-                }
-                
-                if (a < 1e-12|| b < 1e-12) continue;
-                double corr = std::abs(c) / std::sqrt(a*b);
+  while (any_rots && sweeps < max_sweeps) {
+    any_rots = false;
+    for (int i = 0; i < D - 1; ++i) {
+      for (int j = i + 1; j < D; ++j) {
 
-                if (corr > epsilon){
-                    any_rots = true;
-
-                    double tau = (b - a) / (2.0 * c);
-                    double t = sign(tau) / (std::abs(tau) + std::sqrt(1.0 + tau*tau));
-                    double c_rot = 1.0 / std::sqrt(1.0 + t * t);
-                    double s_rot = c_rot * t;
-
-                    for (int r = 0; r < T; ++r){
-                        double x_i = X(r, i);
-                        double x_j = X(r, j);
-                        X(r, i) = c_rot * x_i - s_rot * x_j;
-                        X(r, j) = s_rot * x_i + c_rot * x_j; 
-                    }
-
-                    for (int r = 0; r < D; ++r){
-                        double v_i = V(r, i);
-                        double v_j = V(r, j);
-                        V(r, i) =  c_rot * v_i - s_rot * v_j;
-                        V(r, j) =  s_rot * v_i + c_rot * v_j;
-                    }
-                }
-            }
+        double a = 0.0, b = 0.0, c = 0.0;
+        for (int r = 0; r < T; ++r) {
+          a += X(r, i) * X(r, i);
+          b += X(r, j) * X(r, j);
+          c += X(r, i) * X(r, j);
         }
-        sweeps++;
+
+        if (a < 1e-12 || b < 1e-12)
+          continue;
+        double corr = std::abs(c) / std::sqrt(a * b);
+
+        if (corr > epsilon) {
+          any_rots = true;
+
+          double tau = (b - a) / (2.0 * c);
+          double t = sign(tau) / (std::abs(tau) + std::sqrt(1.0 + tau * tau));
+          double c_rot = 1.0 / std::sqrt(1.0 + t * t);
+          double s_rot = c_rot * t;
+
+          for (int r = 0; r < T; ++r) {
+            double x_i = X(r, i);
+            double x_j = X(r, j);
+            X(r, i) = c_rot * x_i - s_rot * x_j;
+            X(r, j) = s_rot * x_i + c_rot * x_j;
+          }
+
+          for (int r = 0; r < D; ++r) {
+            double v_i = V(r, i);
+            double v_j = V(r, j);
+            V(r, i) = c_rot * v_i - s_rot * v_j;
+            V(r, j) = s_rot * v_i + c_rot * v_j;
+          }
+        }
+      }
     }
-    std::cout << "Converged in " << sweeps << " sweeps.\n\n";
+    sweeps++;
+  }
+  std::cout << "Converged in " << sweeps << " sweeps.\n\n";
 }
 
-void extractUSigma(const MatrixHCI& W, MatrixHCI& U, std::vector<double>& sigma){
-    sigma.resize(W.cols, 0.0);
-    for (int c = 0; c < W.cols; ++c){
-        double norm = 0.0;
-        for (int r = 0; r < W.rows; ++r){
-            norm += W(r,c) * W(r,c);
-        }
-        sigma[c] = std::sqrt(norm);
-
-        for (int r = 0; r < W.rows; ++r){
-            if (sigma[c] > 1e-12){U(r,c) = W(r,c)/sigma[c];}
-            else {U(r,c) = 0.0;}            
-        }      
+inline void extractUSigma(const MatrixHCI &W, MatrixHCI &U,
+                   std::vector<double> &sigma) {
+  sigma.resize(W.cols, 0.0);
+  for (int c = 0; c < W.cols; ++c) {
+    double norm = 0.0;
+    for (int r = 0; r < W.rows; ++r) {
+      norm += W(r, c) * W(r, c);
     }
+    sigma[c] = std::sqrt(norm);
+
+    for (int r = 0; r < W.rows; ++r) {
+      if (sigma[c] > 1e-12) {
+        U(r, c) = W(r, c) / sigma[c];
+      } else {
+        U(r, c) = 0.0;
+      }
+    }
+  }
 }
